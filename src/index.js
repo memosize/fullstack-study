e38324018f9b08ac5ce356f886790cfbfb074118
memosize <89068024@qq.com>
2019-03-14 08:52:23 +0800

    react-router 结合redux 权限控制实现

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import thunk from 'redux-thunk'
+import {createStore,applyMiddleware,compose} from 'redux'
+import {Provider} from 'react-redux'
+import {BrowserRouter as Router,Route,Switch,Redirect} from 'react-router-dom'
+import Auth from './Auth'
+import DashBoard from './DashBoard'
+import reducers  from './reducer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers,compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension?window.devToolsExtension():f=>f
+))
+    ReactDOM.render
+    ((
+        <Provider store={store}>
+            <Router>
+               <Switch>
+                   <Route path='/login' component={Auth}></Route>
+                   <Route path='/dashboard' component={DashBoard}></Route>
+                   <Redirect to='/dashboard'></Redirect>
+               </Switch> 
+            </Router>
+         
+        </Provider>
+    ), document.getElementById('root')  
+    ) 
+    
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
